7d379dbcbb8c6ebaeb7a10860bf8ca6363c17013
Shao <30294610+shaogit@users.noreply.github.com>
2020-01-20 17:27:46 +0800

    Update exception.py

--- ormlite/exception.py
+++ ormlite/exception.py
@@ -1,15 +1,53 @@
 
-class RecordNotExists(Exception):
-	pass
+class ORMLiteException(Exception):
+    # ormlite base exception
+    pass
 
-class MultiRecordError(Exception):
-	pass
 
-class ORMException(Exception):
-	pass
+class FieldException(ORMLiteException):
+    # field base exception
+    pass
 
-class DoseNotExist(Exception):
-	pass
 
-class RecordEmpty(Exception):
-	pass
+class ModelException(ORMLiteException):
+    # model base exception
+    pass
+
+
+class CompilerException(ORMLiteException):
+    # compiler base exception
+    pass
+
+
+class NotExists(ModelException):
+    # record not exists
+    pass
+
+class MultiResult(ModelException):
+    pass
+
+
+class FieldNameException(ORMLiteException):
+    pass
+
+class FieldTypeError(ORMLiteException):
+    pass
+
+class ModelError(ORMLiteException):
+    pass
+
+class RecordNotExists(ORMLiteException):
+    pass
+
+class MultiRecordError(ORMLiteException):
+    pass
+
+class CompileError(ORMLiteException):
+    pass
+
+class InvalidConfiguration(ORMLiteException):
+    pass
+
+
+class AccessorError(ORMLiteException):
+    pass
