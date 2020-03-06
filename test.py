71c558bbf2e6ebf3c3e5593ce35b3b56cc57be68
Shao <30294610+Dev-Shao@users.noreply.github.com>
2020-03-06 18:07:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,47 @@
 import datetime
-from ormlite.base import configuration
-from ormlite.model import Model
-from ormlite import fields
+import logging
+import ormlite
+from ormlite import configuration
 from ormlite.db.utils import create_tables,Min,Count,Sum
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 configuration.conf_db({
     "ENGINE":"ormlite.db.sqlite3",
 	"NAME":"db.sqlite3",
 })
 
+configuration.set_logger(logging)
+configuration.debug = True
 
 
-class User(Model):
-    id = fields.PrimaryKey()
-    name = fields.CharField(max_length=50)
-    sex = fields.CharField(max_length=1)
-    age = fields.IntegerField()
-    birthday = fields.DateField()
+class Order(ormlite.Model):
+    price = ormlite.FloatField()
+    user = ormlite.ForeignKey("User",on_delete=ormlite.CASCADE)
+
+
+class User(ormlite.Model):
+    id = ormlite.PrimaryKey()
+    name = ormlite.CharField(max_length=50)
+    sex = ormlite.CharField(max_length=1)
+    age = ormlite.IntegerField()
+    birthday = ormlite.DateField()
+
+
+
+def aotu_add():
+    return datetime.datetime.now()
+
+def today():
+    return datetime.datetime.now().date()
+
+class Goods(ormlite.Model):
+    number = ormlite.PrimaryKey()
+    name = ormlite.CharField(max_length=100)
+    dt   = ormlite.DateTimeField()
+    time = ormlite.TimeFiled()
+    date = ormlite.DateField()
+
+
+create_tables([Order],configuration.db)
