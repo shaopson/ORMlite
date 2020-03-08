a5fdcd2670381f706d292970e3eddab4d2c86514
LingwenShao <shaopson@outlook.com>
2020-03-07 19:56:46 -0800

     删除多余文件

--- fields/__init__.py
+++
@@ -1,9 +0,0 @@
-from ormlite.fields.base import *
-from ormlite.fields.related import *
-
-
-__all__ = [
-    "Field","BooleanField","CharField","DateField","DateTimeField","FloatField",
-    "IntegerField","TimeFiled","TextField","PrimaryKey","FieldException","RelatedField",
-    "ForeignKey","RelatedDescriptor","CASCADE","SET_NULL","NOT_ACTION","RESTRICT"
-]
