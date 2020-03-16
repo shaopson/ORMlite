4c775ab9766bd2ac42cfcd6a6972ed0bf9ad243f
Shao <30294610+Dev-Shao@users.noreply.github.com>
2020-03-16 16:15:05 +0800

    Update table.py

--- ormlite/db/sqlite3/table.py
+++ ormlite/db/sqlite3/table.py
@@ -1,5 +1,4 @@
-from ormlite.exception import FieldTypeError
-from ormlite.fields import RelatedField
+from ormlite.fields import FieldException
 from .base import Database
 
 
@@ -14,11 +13,11 @@ class Table():
     def __init__(self,model):
         self.model = model
 
-    def get_column_define(self,field):
+    def get_definition(self,field):
         sql = []
         column_type = self.column_types.get(field.get_type())
         if not column_type:
-            raise FieldTypeError("%s Unknown field type:%s" % (field,field.get_type()))
+            raise FieldException("%s Unknown field type:%s" % (field,field.get_type()))
         elif column_type.find("max_length") > 0:
             column_type = column_type % {"max_length":field.max_length}
         sql.append(column_type)
@@ -30,43 +29,52 @@ class Table():
             sql.append("NOT NULL")
         return " ".join(sql)
 
+    def get_constraint(self,fk):
+        field = fk
+        rel_model = field.get_related_model()
+        rel_field = field.get_related_field()
+        const_name = "fk_%s_%s_%s_%s" % (self.model._opts.model_name, field.name,
+                        rel_model._opts.model_name, rel_field.name)
+        constraint = 'CONSTRAINT "%s" FOREIGN KEY("%s") REFERENCES "%s"("%s") ON UPDATE %s ON DELETE %s' % (
+                            const_name, field.get_column(), rel_model._opts.model_name,rel_field.get_column(),
+                            field.on_update,field.on_delete)
+        return constraint
+
     def as_sql_create(self):
         table = []
-        table_name = self.model.__table__
+        table_name = self.model._opts.model_name
         primary_key = []
         foreign_key = []
-        for field in self.model.__fields__.values():
-            column = self.get_column_define(field)
-            table.append("'%s' %s" % (field.name,column))
+        for field in self.model._opts.fields:
+            definition = self.get_definition(field)
+            table.append('"%s" %s' % (field.get_column(),definition))
             if field.primary_key:
-                primary_key.append(repr(field.name))
-            elif isinstance(field,RelatedField):
+                primary_key.append('"%s"' % field.name)
+            elif field.is_related:
                 foreign_key.append(field)
         if primary_key:
-            table.append("PRIMARY KEY(%s)" % ",".join(primary_key))
+            table.append("PRIMARY KEY(%s)" % ','.join(primary_key))
         if foreign_key:
-            constraint = []
             for field in foreign_key:
-                constraint.append("FOREIGN KEY('%s') REFERENCES '%s'('%s')" % (
-                field.name, field.related_model.__table__, field.related_field.name))
-            table.append(",".join(constraint))
-        sql = "CREATE TABLE IF NOT EXISTS %s (\r\n\t%s\r\n);" % (table_name,",\r\n\t".join(table))
+                constraint = self.get_constraint(field)
+                table.append(constraint)
+        sql = 'CREATE TABLE IF NOT EXISTS "%s" (\r\n\t%s\r\n);' % (table_name,",\r\n\t".join(table))
         return sql
 
     def as_sql_delete(self):
-        sql = "DROP TABLE %s;" % self.model.__table__
+        sql = "DROP TABLE %s;" % self.model._opts.model_name
         return sql
 
     def create(self,connection):
         if self.is_existed(connection):
-            raise self.TableExisted("Create fail! <Table:%s> existed" % self.model.__table__)
+            raise self.TableExisted("Create fail! <Table:%s> existed" % self.model._opts.model_name)
         sql = self.as_sql_create()
         print(sql)
         connection.execute(sql)
         connection.commit()
 
     def is_existed(self,connection):
-        sql = 'SELECT "name" FROM sqlite_master WHERE "name" = "%s";' % self.model.__table__
+        sql = 'SELECT "name" FROM sqlite_master WHERE "name" = "%s";' % self.model._opts.model_name
         cursor = connection.execute(sql)
         result = cursor.fetchall()
         if result:
