ea79fa21df2649416e761f164b89b95721fe5905
Shao <30294610+Dev-Shao@users.noreply.github.com>
2020-03-16 16:13:17 +0800

    Update utils.py

--- ormlite/db/utils.py
+++ ormlite/db/utils.py
@@ -1,18 +1,6 @@
 from ormlite.base import configuration
 
 
-Min = lambda x:'MIN("%s")' % x
-
-Max = lambda x:'MAX("%s")' % x
-
-Sum = lambda x:'SUM("%s")' % x
-
-Count = lambda x:'COUNT("%s")' % x
-
-Avg = lambda x:'AVG("%s")' % x
-
-
-
 def create_tables(models,db):
     Table = configuration.db_engine.table.Table
     connection = db.get_connector()
